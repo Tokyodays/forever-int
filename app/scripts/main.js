322bb37a22db0e00b17f1209a87311ddad27fe7d
Tokyodays <kikuchi@gmail.com>
2016-04-24 03:11:15 +0900

    set localize

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -205,6 +205,24 @@
 						}
 					});
 
+		function getQueryVariable(variable) {
+		  var query = window.location.search.substring(1);
+		  var vars = query.split("&");
+		  for (var i=0;i<vars.length;i++) {
+	      var pair = vars[i].split("=");
+	      if (pair[0] == variable) {
+          return pair[1];
+	      }
+		  }
+		}
+
+		var _lang = getQueryVariable("lang");
+		var _obj = ( _lang != "underfined" ) ? { "language": _lang } : {};
+		_obj.pathPrefix = "/language";
+
+		$("[data-localize]").localize("language", _obj);
+
+
 	});
 
 })(jQuery);
